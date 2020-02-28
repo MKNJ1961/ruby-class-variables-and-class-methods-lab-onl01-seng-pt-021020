bac637a65563980ad044651816f461b9bdbc2265
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-28 01:52:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
       @name = song_name
       @artist = artist
       @@genre = genre
-      @@count = count += 1
+      @@count  += 1
       @@genres << genre
       @@artists << artist
 
@@ -28,5 +28,5 @@ class Song
    @@artists.uniq!
  end
 
-
+    
   end
