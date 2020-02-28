0bf180b3365bbc59508dc4eba63b10e1b03e903c
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-28 02:05:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,38 +1,39 @@
-
 class Song
-  attr_accessor :name, :artist, :genre
 
-  @@count = 0
-  @@genres = []
-  @@artists = []
+    attr_accessor :name, :artist, :genre
 
-  def initialize(song_name, artist, genre)
-    @name = song_name
-    @artist = artist
-    @genre = genre
-    @@count += 1
-    @@genres << genre
-    @@artists << artist
-  end
+    @@count = 0
+    @@genres = []
+    @@artists = []
 
-  def self.count
-    @@count
-  end
+    def initialize(song_name, artist, genre)
+      @name = song_name
+      @artist = artist
+      @@genre = genre
+      @@count  += 1
+      @@genres << genre
+      @@artists << artist
 
-  def self.genres
-    @@genres.uniq!
-  end
+    end
 
-  def self.artists
-    @@artists.uniq!
-  end
+    def self.count
+      @@count
+    end
 
-  def self.genre_count
+    def self.genres
+      @@genres.uniq!
+    end
+
+    def self.artists
+      @@artists.uniq!
+    end
+
+    def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre] #if the genre is already in the hash
+      if genre_count[genre]
         genre_count[genre] += 1
-      else #if it's a new genre
+      else
         genre_count[genre] = 1
       end
     end
@@ -50,67 +51,6 @@ class Song
     end
     artist_count
   end
-end
 
 
-
-
-
-
-
-# class Song
-#
-#     attr_accessor :name, :artist, :genre
-#
-#     @@count = 0
-#     @@genres = []
-#     @@artists = []
-#
-#     def initialize(song_name, artist, genre)
-#       @name = song_name
-#       @artist = artist
-#       @@genre = genre
-#       @@count  += 1
-#       @@genres << genre
-#       @@artists << artist
-#
-#     end
-#
-#     def self.count
-#       @@count
-#     end
-#
-#     def self.genres
-#       @@genres.uniq!
-#     end
-#
-#     def self.artists
-#       @@artists.uniq!
-#     end
-#
-#     def self.genre_count
-#     genre_count = {}
-#     @@genres.each do |genre|
-#       if genre_count[genre]
-#         genre_count[genre] += 1
-#       else
-#         genre_count[genre] = 1
-#       end
-#     end
-#     genre_count
-#   end
-#
-#   def self.artist_count
-#     artist_count = {}
-#     @@artists.each do |artist|
-#       if artist_count[artist] #if the artist is already in the hash
-#         artist_count[artist] += 1
-#       else
-#         artist_count[artist] = 1
-#       end
-#     end
-#     artist_count
-#   end
-#
-#
-#   end
+  end
